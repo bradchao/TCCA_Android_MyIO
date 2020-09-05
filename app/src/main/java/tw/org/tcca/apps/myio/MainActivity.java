8fd6e2e088c140e78a7e0e2185e043c52038e2f4
bradchao <brad@brad.tw>
2020-09-05 14:12:03 +0800

    2020090501

---
+++ app/src/main/java/tw/org/tcca/apps/myio/MainActivity.java
@@ -0,0 +1,20 @@
+package tw.org.tcca.apps.myio;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void test1(View view) {
+    }
+    public void test2(View view) {
+    }
+}
