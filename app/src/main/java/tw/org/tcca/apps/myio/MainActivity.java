6fcaec08410efbda4c830de7ddca397638ff81f4
bradchao <brad@brad.tw>
2020-09-05 14:16:49 +0800

    2020090502

--- app/src/main/java/tw/org/tcca/apps/myio/MainActivity.java
+++ app/src/main/java/tw/org/tcca/apps/myio/MainActivity.java
@@ -2,15 +2,21 @@ package tw.org.tcca.apps.myio;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+    private SharedPreferences sp;
+    private SharedPreferences.Editor editor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        sp = getSharedPreferences("config", MODE_PRIVATE);
+        editor = sp.edit();
     }
 
     public void test1(View view) {
