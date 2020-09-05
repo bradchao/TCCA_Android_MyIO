263a778bf778764a8035ebd0550d40f873fa2cf4
bradchao <brad@brad.tw>
2020-09-05 14:28:32 +0800

    2020090503

--- app/src/main/java/tw/org/tcca/apps/myio/MainActivity.java
+++ app/src/main/java/tw/org/tcca/apps/myio/MainActivity.java
@@ -5,21 +5,43 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     private SharedPreferences sp;
     private SharedPreferences.Editor editor;
+    private TextView tv;
+    private String name;
+    private int counter;
+    private boolean sound;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        tv = findViewById(R.id.tv);
+
         sp = getSharedPreferences("config", MODE_PRIVATE);
         editor = sp.edit();
+
+        name = sp.getString("name", "nobody");
+        counter = sp.getInt("counter", 1);
+        sound = sp.getBoolean("sound", true);
+
+        tv.setText(name +":" + counter + ":" + sound);
+        counter++;
+        editor.putInt("counter", counter);
+        editor.commit();
+
     }
 
     public void test1(View view) {
+        name = "Brad";
+        editor.putString("name", name);
+        editor.putBoolean("sound", false);
+        editor.commit();
+
     }
     public void test2(View view) {
     }
