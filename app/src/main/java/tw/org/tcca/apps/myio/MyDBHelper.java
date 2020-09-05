d16c28e791338ae1ccdfd6738c859796515245df
bradchao <brad@brad.tw>
2020-09-05 15:13:31 +0800

    2020090506

---
+++ app/src/main/java/tw/org/tcca/apps/myio/MyDBHelper.java
@@ -0,0 +1,26 @@
+package tw.org.tcca.apps.myio;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class MyDBHelper extends SQLiteOpenHelper {
+    private String createTable =
+            "CREATE TABLE cust (id INTEGER PRIMARY KEY AUTOINCREMENT,cname TEXT,tel TEXT)";
+
+    public MyDBHelper(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        sqLiteDatabase.execSQL(createTable);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
+}
